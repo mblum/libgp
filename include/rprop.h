a25263a7ec836850bfa02cd56b2ce767967fe47e
mblum <none@none>
2011-06-06 12:33:37 +0200

    copyright notice

--- include/rprop.h
+++ include/rprop.h
@@ -1,16 +1,18 @@
-//
-// libgp - Gaussian Process library for Machine Learning
-// Copyright (C) 2010 Universität Freiburg
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+/**************************************************************
+libgp - Gaussian Process library for Machine Learning
+Copyright (C) 2011 Universität Freiburg
+Author: Manuel Blum
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+***************************************************************/
 
 #ifndef __RPROP_H__
 #define __RPROP_H__
--- tests/gp_regression_test.cc
+++ tests/gp_regression_test.cc
@@ -1,23 +1,24 @@
-//
-// libgp - Gaussian Process library for Machine Learning
-// Copyright (C) 2010 Universität Freiburg
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+/**************************************************************
+libgp - Gaussian Process library for Machine Learning
+Copyright (C) 2011 Universität Freiburg
+Author: Manuel Blum
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+***************************************************************/
 
 #define F(X, Y) (X * exp(-X*X - Y*Y))
 
 #include "gp.h"
 #include "cov_factory.h"
-
-#include "cmath"
+#include <cmath>
 #include <iostream>
 #include <gtest/gtest.h>
 
