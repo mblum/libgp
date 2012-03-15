2fa46e497d4f735200bc22b203ae98084f7fac91
Manuel Blum <mblum@informatik.uni-freiburg.de>
2012-03-15 11:57:21 +0100

    change FindEigen script, such that clsquare version is found first

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -63,10 +63,10 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
+      ${CMAKE_CURRENT_SOURCE_DIR}/../ThirdParty/Eigen3
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}      
       $ENV{HOME}/local/include
-      ${CMAKE_CURRENT_SOURCE_DIR}/../ThirdParty/Eigen3
       PATH_SUFFIXES eigen3 eigen
   )
 
