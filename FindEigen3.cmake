fdbdd4877be9a12935690c8851dd4593463ea51f
Manuel Blum <mblum@informatik.uni-freiburg.de>
2012-07-19 17:55:40 +0200

    remove CLSquare specific folder in findscript

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -63,7 +63,6 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
-      ${CMAKE_CURRENT_SOURCE_DIR}/../ThirdParty/Eigen3
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}      
       $ENV{HOME}/local/include
