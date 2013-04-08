094c51bc1df3e3ee504ee24426838c049b4cd8b5
Manuel Blum <mail@bluman.de>
2013-04-08 17:34:31 +0200

    adding missing files

---
+++ include/cov_periodic_matern3_iso.h
@@ -0,0 +1,31 @@
+// libgp - Gaussian process library for Machine Learning
+// Copyright (c) 2011, Manuel Blum <mblum@informatik.uni-freiburg.de>
+// All rights reserved.
+
+#ifndef __COV_PERIODIC_MATERN3_ISO_H__
+#define __COV_PERIODIC_MATERN3_ISO_H__
+
+#include "cov.h"
+
+namespace libgp
+{
+  class CovPeriodicMatern3iso : public CovarianceFunction
+  {
+  public:
+    CovPeriodicMatern3iso ();
+    virtual ~CovPeriodicMatern3iso ();
+    bool init(int n);
+    double get(const Eigen::VectorXd &x1, const Eigen::VectorXd &x2);
+    void grad(const Eigen::VectorXd &x1, const Eigen::VectorXd &x2, Eigen::VectorXd &grad);
+    void set_loghyper(const Eigen::VectorXd &p);
+    virtual std::string to_string();
+  private:
+    double ell;
+    double sf2;
+    double sqrt3;
+    double T;
+  };
+  
+}
+
+#endif
