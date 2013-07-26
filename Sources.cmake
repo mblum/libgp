6bdcf6352f764761f01f6c889cff3052070dfa66
Manuel Blum <mblum@informatik.uni-freiburg.de>
2013-07-26 16:06:49 +0200

    adding tests and compile periodic covariance function

--- Sources.cmake
+++ Sources.cmake
@@ -8,6 +8,7 @@ SET(LIBGP_SRC
   src/cov_noise.cc
   src/cov_rq_iso.cc
   src/cov_periodic_matern3_iso.cc
+  src/cov_periodic.cc
   src/cov_se_ard.cc
   src/cov_se_iso.cc
   src/cov_sum.cc
@@ -30,6 +31,7 @@ SET(LIBGP_INTERFACES
   include/cov_noise.h
   include/cov_rq_iso.h
   include/cov_periodic_matern3_iso.h
+  include/cov_periodic.h
   include/cov_se_ard.h
   include/cov_se_iso.h
   include/cov_sum.h
