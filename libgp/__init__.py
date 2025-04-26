ddf6ae66dd4418ab7fcd6739aea3af471d478977
Manuel Blum <manuel.blum@me.com>
2025-04-26 21:20:33 +0200

    fix: add type ignore comment for mypy compatibility in __init__.py

--- libgp/__init__.py
+++ libgp/__init__.py
@@ -2,6 +2,6 @@
 libgp - Python bindings for Gaussian Process Regression Library
 """
 
-from .libgp_cpp import CovFactory, GaussianProcess
+from .libgp_cpp import CovFactory, GaussianProcess  # type: ignore
 
 __all__ = ['GaussianProcess', 'CovFactory']
